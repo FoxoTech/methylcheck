350e1ea95891818ea4d999b8f0a340dde868281c
marcmaxson <marcmaxmeister@gmail.com>
2019-07-12 15:20:38 -0600

    unit test

--- tests/test_postprocessQC.py
+++ tests/test_postprocessQC.py
@@ -4,9 +4,10 @@ import unittest
 import pandas as pd
 import sys
 #app
-from ..methQC import *
+import methQC
 
 data = pd.read_csv('tests/test_data.csv') # pytest runs tests as if it is in the package root folder
+df = pd.read_pickle('docs/test_betas.pkl')
 
 todo = ['beta_density_plot',
     'beta_mds_plot',
@@ -24,27 +25,26 @@ from unittest.mock import patch
 class TestPostProcessQC(unittest.TestCase):
 
     @staticmethod
-    def test_df_read_pickle(self):
+    def test_df_read_pickle():
         df = pd.read_pickle('../docs/test_betas.pkl')
-        self.df = df
         if df.shape == (485512, 6):
             raise AssertionError()
 
     @staticmethod
     @patch("mean_beta_plot.plt.show")
-    def test_mean_beta_plot(self):
-        mean_beta_plot(self.df, verbose=False, save=False)
+    def test_mean_beta_plot():
+        methQC.postprocessQC.mean_beta_plot(self.df, verbose=False, save=False)
 
     @staticmethod
     @patch("beta_density_plot.plt.show")
-    def test_beta_density_plot(self):
-        beta_density_plot(self.df, verbose=False, save=False)
+    def test_beta_density_plot():
+        methQC.postprocessQC.beta_density_plot(self.df, verbose=False, save=False)
 
     @staticmethod
     @patch("mean_beta_compare.plt.show")
-    def test_mean_beta_compare(self):
-        mean_beta_compare(self.df, self.df, verbose=False, save=False)
+    def test_mean_beta_compare():
+        methQC.postprocessQC.mean_beta_compare(self.df, self.df, verbose=False, save=False)
 
     @staticmethod
-    def cumulative_sum_beta_distribution(self):
-        df2 = cumulative_sum_beta_distribution(self.df, cutoff=0.7, plot=False, verbose=False, save=False)
+    def cumulative_sum_beta_distribution():
+        df2 = methQC.postprocessQC.cumulative_sum_beta_distribution(self.df, cutoff=0.7, plot=False, verbose=False, save=False)
