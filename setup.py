e4e2414424bae7fecbe5b6c562b34dd7c7b55ae8
marcmaxson <marcmaxmeister@gmail.com>
2019-08-22 12:28:36 -0600

    won't import on command line

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ See https://life-epigenetics-methylcheck.readthedocs-hosted.com/en/latest/ for f
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     project_urls = {
-        "Documentation": "https://life-epigenetics-methylprep.readthedocs-hosted.com/en/latest/",
-        "Source": "https://github.com/lifeEGX/methylprep/",
+        "Documentation": "https://life-epigenetics-methylcheck.readthedocs-hosted.com/en/latest/",
+        "Source": "https://github.com/lifeEGX/methylcheck/",
         "Funding": "https://lifeegx.com/"
     },    
     classifiers=[
