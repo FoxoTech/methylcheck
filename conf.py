225cff08493502de483d31fbf86ff9dde78008e1
Marc Maxmeister <mmaxmeister@lifeegx.com>
2019-09-06 16:55:02 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -49,7 +49,7 @@ extensions = [
 apidoc_module_dir = '.'
 apidoc_output_dir = '_sphinx_build'
 apidoc_excluded_paths = ['tests']
-apidoc_separate_modules = False
+apidoc_separate_modules = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_sphinx_templates']
