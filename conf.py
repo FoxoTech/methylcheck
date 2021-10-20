8c3d94cfcf281e156958b72ee8ff38eb757491be
Marc Maxmiester <marcmaxmeister@gmail.com>
2021-10-20 14:09:18 -0600

    version fix

--- conf.py
+++ conf.py
@@ -26,7 +26,7 @@ author = 'FOXO Technologies, inc.'
 # The short X.Y version
 version = '0.7'
 # The full version, including alpha/beta/rc tags
-release = '0.7.6'
+release = '0.7.7'
 
 
 # -- General configuration ---------------------------------------------------
